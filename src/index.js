7c925b06e0d28540d7631e4c12939c2bb491790a
Stefanni Brasil <stefannibrasil@gmail.com>
2019-02-02 16:01:41 -0800

    Fix typo

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@
 	// render App component into app-root element
 
 
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+
+import App from "./App.jsx"
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app-root")
+)
