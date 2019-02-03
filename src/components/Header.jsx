4b8f08740a3e0b65be425656e11a73f738623dd7
Stefanni Brasil <stefannibrasil@gmail.com>
2019-02-03 10:54:34 -0800

    Add logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,29 @@
 // Exercise 2: Set It Up
-    // write a class component
-    // make it a ul with a class name of Header
-    // make four li: Catstagram, Home, About, Profile
-    // put class name of Logo on the li element with GDInsta
-    // export default the component!
+// write a class component
+// make it a ul with a class name of Header
+// make four li: Catstagram, Home, About, Profile
+// put class name of Logo on the li element with GDInsta
+// export default the component!
 
 // Exercise 6: Routing
-    // import Link from react-router-dom
-    // use the Link component to link to each route with a 'to' property
-    // check that your links work!
+// import Link from react-router-dom
+// use the Link component to link to each route with a 'to' property
+// check that your links work!
 
-import React from 'react';
-
+import React from "react";
 
 class Header extends React.Component {
   render() {
     return (
-      <ul className='Header'>
+      <ul className="Header">
+        <li className="Logo">GDInsta</li>
         <li>Catstagram</li>
         <li>Home</li>
         <li>About</li>
         <li>Profile</li>
       </ul>
-    )
+    );
   }
 }
 
-export { Header as default }
-
+export { Header as default };
